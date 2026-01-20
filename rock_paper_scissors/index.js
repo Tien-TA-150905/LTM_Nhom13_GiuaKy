00e23561a3576824976040b2d5d970de71f88119
Tien-TA-150905 <tienta3014@ut.edu.vn>
2026-01-20 23:08:53 +0700

    20-1-2026

--- rock_paper_scissors/index.js
+++ rock_paper_scissors/index.js
@@ -6,6 +6,8 @@ const server = http.createServer(app);
 const {Server} = require("socket.io");
 const io = new Server(server);
 
+const rooms = {};
+
 app.use(express.static(path.join(__dirname, 'client')));
 
 
@@ -21,9 +23,34 @@ io.on('connection', (socket) => {
     console.log('a user connected!');
     socket.on('disconnect', () => {
         console.log('user disconnect!')
+
     });
+    socket.on('createGame', () => {
+        const roomUniqueId = makeid(6);
+        rooms[roomUniqueId] = {};
+        socket.join(roomUniqueId);
+        socket.emit("newGame", {roomUniqueId: roomUniqueId})
+    });
+
+    socket.on('joinGame', (data) => {
+        if(rooms[data.roomUniqueId] != null){
+            socket.join(data.roomUniqueId);
+            socket.to(data.roomUniqueId).emit("playersConnected", {});
+            socket.emit("playersConnected");
+        }
+    })
 });
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
 });
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
