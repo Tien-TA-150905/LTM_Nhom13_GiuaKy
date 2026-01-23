ab96db7f82da7eb35ada3fe3b1ee90bf2b0090bf
Tien-TA-150905 <tienta3014@ut.edu.vn>
2026-01-23 11:24:06 +0700

    23-1-2026

--- rock_paper_scissors/client/client.js
+++ rock_paper_scissors/client/client.js
@@ -2,8 +2,10 @@ console.log("client.js excuting");
 
 const socket = io();
 let roomUniqueId = null;
+let player1 = false;
 
 function createGame() {
+    player1 = true;
     socket.emit('createGame');
 }
 
@@ -30,10 +32,74 @@ socket.on("newGame", (data) => {
 });
 
 socket.on("playersConnected", () => {
+    document.getElementById('initial').style.display = 'none';
     document.getElementById('waitingArea').style.display = 'none';
     document.getElementById('gameArea').style.display = 'block';
 })
 
-function sendChoice(rpsChoice) {
+socket.on("p1Choice", (data) => {
+    if (!player1) {
+       createOpponentChoiceButton(data);
+    }
+});
 
+socket.on("p2Choice", (data) => {
+    if (player1) {
+       createOpponentChoiceButton(data);
+    }
+});
+
+socket.on("result", (data) => {
+    let winnerText = '';
+    if (data.winner != 'd'){
+        if (data.winner == 'p1' && player1){
+            winnerText = 'You win';
+        }
+        else if (data.winner == 'p1'){
+            winnerText = 'You lose';
+        }
+        else if (data.winner == 'p2' && !player1){
+            winnerText = 'You win';
+        }
+        else if (data.winner == 'p2'){
+            winnerText = 'You lose';
+        }
+    }
+    else {
+        winnerText = 'DRAW';
+    }
+    document.getElementById('opponentState').style.display = 'none';
+    document.getElementById('opponentButton').style.display = 'block';
+    document.getElementById('winnerArea').innerHTML = winnerText;
+});
+
+function sendChoice(rpsValue) {
+    const choiceEvent = player1 ? "p1Choice" : "p2Choice";
+
+    socket.emit(choiceEvent, {
+        rpsValue: rpsValue,
+        roomUniqueId: roomUniqueId
+    });
+
+    let playerChoiceButton = document.createElement('button');
+    playerChoiceButton.className = rpsValue.toLowerCase();
+    playerChoiceButton.disabled = true;
+
+    document.getElementById('player1Choice').innerHTML = "";
+    document.getElementById('player1Choice').appendChild(playerChoiceButton);
+}
+
+function createOpponentChoiceButton(data) {
+    document.getElementById('opponentState').innerHTML = "Opponent made a choice";
+
+    let opponentButton = document.getElementById('opponentButton');
+    if (!opponentButton) {
+        opponentButton = document.createElement('button');
+        opponentButton.id = 'opponentButton';
+        opponentButton.disabled = true;
+        document.getElementById('player2Choice').appendChild(opponentButton);
+    }
+
+    opponentButton.className = data.rpsValue.toLowerCase(); 
+    opponentButton.style.display = 'none'; 
 }
--- rock_paper_scissors/index.js
+++ rock_paper_scissors/index.js
@@ -20,11 +20,11 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected!');
+    console.log('a user connected');
     socket.on('disconnect', () => {
-        console.log('user disconnect!')
-
+        console.log('user disconnected');
     });
+
     socket.on('createGame', () => {
         const roomUniqueId = makeid(6);
         rooms[roomUniqueId] = {};
@@ -33,13 +33,69 @@ io.on('connection', (socket) => {
     });
 
     socket.on('joinGame', (data) => {
-        if(rooms[data.roomUniqueId] != null){
+        if(rooms[data.roomUniqueId] != null) {
             socket.join(data.roomUniqueId);
             socket.to(data.roomUniqueId).emit("playersConnected", {});
             socket.emit("playersConnected");
         }
     })
+
+    socket.on("p1Choice",(data)=>{
+        let rpsValue = data.rpsValue;
+        rooms[data.roomUniqueId].p1Choice = rpsValue;
+        socket.to(data.roomUniqueId).emit("p1Choice",{rpsValue : data.rpsValue});
+        if(rooms[data.roomUniqueId].p2Choice != null) {
+            declareWinner(data.roomUniqueId);
+        }
+    });
+
+    socket.on("p2Choice",(data)=>{
+        let rpsValue = data.rpsValue;
+        rooms[data.roomUniqueId].p2Choice = rpsValue;
+        socket.to(data.roomUniqueId).emit("p2Choice",{rpsValue : data.rpsValue});
+        if(rooms[data.roomUniqueId].p1Choice != null) {
+            declareWinner(data.roomUniqueId);
+        }
+    });
 });
+
+function declareWinner (roomUniqueId){
+    let p1Choice = rooms[roomUniqueId].p1Choice;
+    let p2Choice = rooms[roomUniqueId].p2Choice;
+    let winner = null;
+    if (p1Choice === p2Choice) {
+        winner = "d";
+    }
+    else if (p1Choice == "Paper") {
+        if (p2Choice == "Scissor") {
+            winner = "p2";
+        }
+        else {
+            winner = "p1";
+        }
+    }
+    else if (p1Choice == "Rock") {
+        if (p2Choice == "Paper") {
+            winner = "p2";
+        }
+        else {
+            winner = "p1";
+        }
+    }
+    else if (p1Choice == "Scissor") {
+        if (p2Choice == "Rock") {
+            winner = "p2";
+        }
+        else {
+            winner = "p1";
+        }
+    }
+    io.sockets.to(roomUniqueId).emit("result", {
+        winner: winner
+    });
+    rooms[roomUniqueId].p1Choice = null;
+    rooms[roomUniqueId].p2Choice = null;
+}
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
